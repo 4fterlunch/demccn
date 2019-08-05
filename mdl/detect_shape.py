c54d6fa3031235de14a211527418b1432eb34103
michael <mholmes2@student.unimelb.edu.au>
2019-08-05 12:15:01 +1000

    fix path issue

--- mdl/detect_shape.py
+++ mdl/detect_shape.py
@@ -1,8 +1,11 @@
 import numpy as np
 import cv2
+import os
 #from matplotlib import pyplot as plt
 
-img = cv2.imread('img_test.jpg', 0)
+dir = os.path.dirname(__file__)
+
+img = cv2.imread(os.path.join(dir,'img_test.jpg'), 0)
 
 cv2.imshow('image', img)
 cv2.waitKey(0)
